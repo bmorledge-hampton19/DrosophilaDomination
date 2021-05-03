a71552c96a78d314e20210b72dbfdf42cff9269b
bmorledge-hampton19 <b.morledge-hampton@wsu.edu>
2021-05-02 18:01:42 -0700

    I should have documented these changes earlier
    I think this is a start on a manager to display different upgrade subcategories...  I think it was unfinished, so hopefully I'll finish that now!  I just wanted to make this commit before the big commit from updating to the most recent Unity LTS edition.

---
+++ Assets/Scripts/Upgrades/UpgradeCategoriesPanelManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeCategoriesPanelManager : MonoBehaviour
+{
+    
+    public GameObject jarsSubcategoriesPanel;
+    public GameObject tasksSubcategoriesPanel;
+    public GameObject miscSubcategoriesPanel;
+
+    GameObject currentSubcategoriesPanel;
+
+    void update() {
+
+    }
+
+    void Awake() {
+
+        currentSubcategoriesPanel = jarsSubcategoriesPanel;
+        focusJarsSubcategoriesPanel();
+
+    }
+
+    public void focusJarsSubcategoriesPanel() {
+        currentSubcategoriesPanel.SetActive(false);
+        jarsSubcategoriesPanel.SetActive(true);
+        currentSubcategoriesPanel = jarsSubcategoriesPanel;
+    }
+    public void focusTasksSubcategoriesPanel() {
+        currentSubcategoriesPanel.SetActive(false);
+        tasksSubcategoriesPanel.SetActive(true);
+        currentSubcategoriesPanel = tasksSubcategoriesPanel;
+    }
+    public void focusMiscSubcategoriesPanel() {
+        currentSubcategoriesPanel.SetActive(false);
+        miscSubcategoriesPanel.SetActive(true);
+        currentSubcategoriesPanel = miscSubcategoriesPanel;
+    }
+
+}
